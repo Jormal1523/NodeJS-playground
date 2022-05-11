3629cd84c86aa1f2075658b99a63d41c9466d07e
Jason Tseng <jormal1523@gmail.com>
2022-05-11 11:36:49 +0800

    補充: coding style

---
+++ app2.js
@@ -0,0 +1,14 @@
+//第一個區塊 內建模組
+const path = require('path');
+const http = require('http');
+
+//第二個區塊 第三方模組（套件）
+
+//第三個區塊 自建模組
+const hello = require('./hello');
+
+////////////////////////////////////////////////////////////////
+
+hello.sayHello();
+
+console.log(hello.title);
