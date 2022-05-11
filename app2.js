da34a3fe4797ad45a1f80ab4a771c4e2f35bd933
Jason Tseng <jormal1523@gmail.com>
2022-05-11 13:39:39 +0800

    cow-say套件

--- app2.js
+++ app2.js
@@ -25,7 +25,7 @@ let sentences = ['Hello', 'World', 'I\'m a cow.'];
 sentences.forEach((sentence) => {
     console.log(cowsay.say({
         text : sentence,
-        e : "^^",
+        e : "^^", 
         T : "U "
     }));
 });
