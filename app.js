403e57d39fa8b915f27b5d644df1bd58330afc80
Jason Tseng <jormal1523@gmail.com>
2022-05-11 11:11:00 +0800

    建立hello模組，並在app.js上使用

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+//匯入hello這個模組（檔案）
+const hello = require('./hello');
+
+// hello.say();
+hello.sayHello();
+hello.sayGoodnight();
+console.log(hello.title)
---
+++ hello.js
@@ -0,0 +1,24 @@
+// hello.js
+//製作一個hello模組
+const sayHello = () => {
+    console.log('Hello!');
+};
+const sayGoodnight = () => {
+    console.log('Good night!');
+};
+module.exports.say = sayHello;
+
+
+//用物件形式製作hello模組
+module.exports = {
+    sayHello: sayHello,
+    sayGoodnight:sayGoodnight,
+    title: 'Hello Title'
+};
+
+//JS remarks
+module.exports = {
+    sayHello,
+    sayGoodnight,
+    title: 'Hello Title'
+};
